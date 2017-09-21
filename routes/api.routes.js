3ff6858d3996d803991398025d83db0101a67bd1
ridmal <ridmal19@gmail.com>
2017-09-21 13:54:45 +0530

    add services for battingScore

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const user = require('./api/users.routes');
 const team = require('./api/teams.routes');
 const player = require('./api/players.routes');
+const battingScore = require('./api/battingScore.routes');
 
 router.use('/user', user);
 router.use('/team',team);
 router.use('/player',player);
+router.use('/battingScore',battingScore);
 
 module.exports = router;
---
+++ services/battingScore.service.js
@@ -0,0 +1,47 @@
+const Q = require('q');
+const databaseService = require('../utils/database.service');
+
+const service = {};
+
+service.addScore = function (args) { // add score
+  const def = Q.defer();
+  const query = `INSERT INTO battingscore (teamId,matchId,playerId,runs,inningId) VALUES (${args.teamId},${args.matchId},${args.playerId},${args.runs},${args.inningId})`;
+  databaseService.addQuery(query)
+    .then((results) => {
+       def.resolve(results);
+          })
+    .catch((error) => {
+      def.reject(error);
+    });
+
+  return def.promise;
+};
+service.getPlayerAllScore = function (args) { // add player 
+  const def = Q.defer();
+  const query = `SELECT b.matchId,m.name,b.runs from battingscore b ,matches m where b.playerId=${args.playerId} and b.matchId=m.matchId`;
+  databaseService.selectQuery(query)
+    .then((results) => {
+       def.resolve(results);
+          })
+    .catch((error) => {
+      def.reject(error);
+    });
+
+  return def.promise;
+};
+service.matchBattingSummery = function (args) { // add player 
+  const def = Q.defer();
+  console.log('cdc');
+  const query = `SELECT p.name,t.teamName,b.playerid,SUM(b.runs) as runs from battingscore b,teams t,players p where b.playerId = p.playerId and b.teamId = t.teamId and b.matchId=${args.matchId} GROUP BY b.matchId,b.playerId`;
+  databaseService.selectQuery(query)
+    .then((results) => {
+       def.resolve(results);
+          })
+    .catch((error) => {
+      def.reject(error);
+    });
+
+  return def.promise;
+};
+
+module.exports = service;
