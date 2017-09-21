6e20faf85e310d361fc13559ed01abfe0dcfb9ef
ridmal <ridmal19@gmail.com>
2017-09-22 03:57:59 +0530

    create databse pool

--- utils/database.service.js
+++ utils/database.service.js
@@ -2,18 +2,27 @@ var mysql = require('mysql');
 const Q = require('q');
 const service = {};
 
-var con = mysql.createConnection({
-  host: "in-cdbr-azure-south-c.cloudapp.net",
-  user: "b8c58532399431",
-  password: "0cc0b94c",
-  database: "fitsixes",
-  port:"3306",
-  ssl : true
+// var con = mysql.createConnection({
+//   host: "in-cdbr-azure-south-c.cloudapp.net",
+//   user: "b8c58532399431",
+//   password: "0cc0b94c",
+//   database: "fitsixes",
+//   port:"3306",
+//   ssl : true
+// });
+var pool  = mysql.createPool({
+  connectionLimit : 4,
+  host            : "in-cdbr-azure-south-c.cloudapp.net",
+  user            : "b8c58532399431",
+  password        : "0cc0b94c",
+  database        : "fitsixes",
+  port            : "3306",
+  ssl             : true,
+  waitForConnections : true
 });
-
-  con.connect(function(err) {
-  if (err) throw err;
-});
+//   con.connect(function(err) {
+//   if (err) throw err;
+// });
 
 
 // service.createTable = function(name){
@@ -23,46 +32,69 @@ var con = mysql.createConnection({
 //         console.log("Table created");
 //     });
 // };
+// pool.getConnection(function(err, connection) {
+//   // Use the connection
+//   connection.query('SELECT something FROM sometable', function (error, results, fields) {
+//     connection.release();
+//     if (error) throw error;
+//   });
+// });
+
 service.selectQuery = function(query){ // select query 
   const def = Q.defer();
-      con.query(query,function(err,result){
+pool.getConnection(function(err, connection) {
+      connection.query(query,function(err,result){
+          connection.release();
          if (err) def.reject(err);
          else def.resolve(result);
     });
+});
   return def.promise;
 }
 service.addQuery = function(query){ // insert query 
     const def = Q.defer();
-      con.query(query,function(err,result){
+    pool.getConnection(function(err, connection) {
+      connection.query(query,function(err,result){
+         connection.release();
          if (err) def.reject(err);
          else def.resolve("successfully added");
+    });
     });
   return def.promise;
 }
 
 service.deleteQuery = function(query){ // insert query 
     const def = Q.defer();
-      con.query(query,function(err,result){
+    pool.getConnection(function(err, connection) {
+      connection.query(query,function(err,result){
+         connection.release();
          if (err) def.reject(err);
          else def.resolve("successfully removed");
+    });
     });
   return def.promise;
 }
 
 service.bulkInsert = function(query,values){ // insert query 
     const def = Q.defer();
-      con.query(query,[values],function(err,result){
+    pool.getConnection(function(err, connection) {
+      connection.query(query,[values],function(err,result){
+         connection.release();
          if (err) def.reject(err);
          else def.resolve("successfully added");
+    });
     });
   return def.promise;
 }
 
 service.updateQuery = function(query){ // update query
     const def = Q.defer();
-      con.query(query,function(err,result){
+    pool.getConnection(function(err, connection) {
+      connection.query(query,function(err,result){
+         connection.release();
          if (err) def.reject(err);
          else def.resolve("successfully Updated");
+    });
     });
   return def.promise;
 }
