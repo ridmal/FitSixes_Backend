85bdfa413bbcabd28beecc574ef9dbe91a823591
Nimesha Buddhika <nimeshabuddhika3@gmail.com>
2017-09-22 15:56:21 +0530

    chnages in db file

--- utils/database.service.js
+++ utils/database.service.js
@@ -2,76 +2,72 @@ var mysql = require('mysql');
 const Q = require('q');
 const service = {};
 
-var con = mysql.createConnection(
-    {
-        host: "in-cdbr-azure-south-c.cloudapp.net",
-        user: "b8c58532399431",
-        password: "0cc0b94c",
-        database: "fitsixes",
-        port: "3306",
-        ssl: true
-
-        /*host: "127.0.0.1",
-         user: "root",
-         password: "",
-         database: "fitsixes",
-         port:"3306"*/
-
-    });
-
-
-con.connect(function (err) {
-    if (err) throw err;
+var pool  = mysql.createPool({
+    connectionLimit : 4,
+    host            : "in-cdbr-azure-south-c.cloudapp.net",
+    user            : "b8c58532399431",
+    password        : "0cc0b94c",
+    database        : "fitsixes",
+    port            : "3306",
+    ssl             : true,
+    waitForConnections : true
 });
 
-
-// service.createTable = function(name){
-//     con.query('CREATE TABLE '+name+' (name VARCHAR(255), address VARCHAR(255))',function(err,result){
-//          if (err) throw err;
-
-//         console.log("Table created");
-//     });
-// };
-service.selectQuery = function (query) { // select query
+service.selectQuery = function(query){ // select query
     const def = Q.defer();
-    con.query(query, function (err, result) {
-        if (err) def.reject(err);
-        else def.resolve(result);
+    pool.getConnection(function(err, connection) {
+        connection.query(query,function(err,result){
+            connection.release();
+            if (err) def.reject(err);
+            else def.resolve(result);
+        });
     });
     return def.promise;
 }
-service.addQuery = function (query) { // insert query
+service.addQuery = function(query){ // insert query
     const def = Q.defer();
-    con.query(query, function (err, result) {
-        if (err) def.reject(err);
-        else def.resolve("successfully added");
-    });
-    return def.promise;
-}
-
-service.deleteQuery = function (query) { // insert query
-    const def = Q.defer();
-    con.query(query, function (err, result) {
-        if (err) def.reject(err);
-        else def.resolve("successfully removed");
+    pool.getConnection(function(err, connection) {
+        connection.query(query,function(err,result){
+            connection.release();
+            if (err) def.reject(err);
+            else def.resolve("successfully added");
+        });
     });
     return def.promise;
 }
 
-service.bulkInsert = function (query, values) { // insert query
+service.deleteQuery = function(query){ // insert query
     const def = Q.defer();
-    con.query(query, [values], function (err, result) {
-        if (err) def.reject(err);
-        else def.resolve("successfully added");
+    pool.getConnection(function(err, connection) {
+        connection.query(query,function(err,result){
+            connection.release();
+            if (err) def.reject(err);
+            else def.resolve("successfully removed");
+        });
     });
     return def.promise;
 }
 
-service.updateQuery = function (query) { // update query
+service.bulkInsert = function(query,values){ // insert query
     const def = Q.defer();
-    con.query(query, function (err, result) {
-        if (err) def.reject(err);
-        else def.resolve("successfully Updated");
+    pool.getConnection(function(err, connection) {
+        connection.query(query,[values],function(err,result){
+            connection.release();
+            if (err) def.reject(err);
+            else def.resolve("successfully added");
+        });
+    });
+    return def.promise;
+}
+
+service.updateQuery = function(query){ // update query
+    const def = Q.defer();
+    pool.getConnection(function(err, connection) {
+        connection.query(query,function(err,result){
+            connection.release();
+            if (err) def.reject(err);
+            else def.resolve("successfully Updated");
+        });
     });
     return def.promise;
 }
